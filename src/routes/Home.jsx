ed1ec0dd53bd2fddfd9a9c52026efff23e6a7c1b
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2023-04-15 15:57:19 +0530

    final

---
+++ src/routes/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CoinSearch from '../components/CoinSearch'
+import Tranding from '../components/Tranding'
+
+
+const Home = ({coins}) => {
+  return (
+    <div>
+      <CoinSearch coins={coins} />
+      <Tranding />
+      
+    </div>
+  )
+}
+
+export default Home
